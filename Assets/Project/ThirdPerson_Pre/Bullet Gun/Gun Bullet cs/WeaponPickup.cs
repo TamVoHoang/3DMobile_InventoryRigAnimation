53dfd68a7ba7d634b0ef23fd8bc4ab52f8aa4ed1
TamVoHoang <130343021+TamVoHoang@users.noreply.github.com>
2024-01-23 17:55:33 +0700

    Jan 23 - 23

--- Assets/Project/ThirdPerson_Pre/Bullet Gun/Gun Bullet cs/WeaponPickup.cs
+++ Assets/Project/ThirdPerson_Pre/Bullet Gun/Gun Bullet cs/WeaponPickup.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 public class WeaponPickup : MonoBehaviour
 {
     //? instantiate gun prefab on player
-    [SerializeField] private RaycastWeapon weaponPrefab;
+    [SerializeField] private RaycastWeapon gunPrefab;
 
     private void OnTriggerEnter(Collider other) {
         ActiveGun activeGun = other.GetComponent<ActiveGun>();
-        int weaponSlotIndex = (int)weaponPrefab.weaponSlot; //=0
+        int weaponSlotIndex = (int)gunPrefab.weaponSlot; //=0
 
         if(activeGun)
         {
-            RaycastWeapon newWeapon = Instantiate(weaponPrefab, activeGun.weaponSlots[weaponSlotIndex].position,
+            RaycastWeapon newWeapon = Instantiate(gunPrefab, activeGun.weaponSlots[weaponSlotIndex].position,
                 activeGun.weaponSlots[weaponSlotIndex].transform.rotation, activeGun.weaponSlots[weaponSlotIndex]);
                 
             newWeapon.transform.SetParent(activeGun.weaponSlots[weaponSlotIndex], false);
-
             activeGun.Equip(newWeapon);
 
             //Destroy(gameObject);
