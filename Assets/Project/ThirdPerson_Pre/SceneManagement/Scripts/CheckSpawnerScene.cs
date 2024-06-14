00fbe35ed07af3997e4bd734e18d4bc7592a516d
TamVoHoang <130343021+TamVoHoang@users.noreply.github.com>
2024-06-14 18:41:33 +0700

    Map selection
    adding scroll view for map select
    touch map - set map select index -> switch (map Index) -> run scene name

--- Assets/Project/ThirdPerson_Pre/SceneManagement/Scripts/CheckSpawnerScene.cs
+++ Assets/Project/ThirdPerson_Pre/SceneManagement/Scripts/CheckSpawnerScene.cs
@@ -2,16 +2,18 @@ using UnityEngine.SceneManagement;
 
 public static class CheckSpawnerScene
 {
-    // not game scenes
+    // not in game scenes
     public const string Intro = "Intro";
     public const string MainMenuScene = "MainMenu";
-    public const string DataOverviewScene = "AccountDataOverview";
+    public const string AccountDataOverview = "AccountDataOverview";
     public const string SpawnerScene = "Testing_SpawnPlayer";
 
     // Game scenes
-    public const string ThirdPerson = "ThirdPerson";
     public const string Testing_ThirdPerson = "Testing_ThirdPerson";
     public const string Testing_BattleRoyale = "Testing_BattleRoyale";
+
+    // scene with having player
+    public const string ThirdPerson = "ThirdPerson";
 
     public static bool CheckScene(string sceneName) {
         if(SceneManager.GetActiveScene().name == sceneName) return true;
@@ -26,7 +28,7 @@ public static class CheckSpawnerScene
     }
 
     public static bool IsInMenuScene() {
-        if(CheckScene(Intro) || CheckScene(MainMenuScene) || CheckScene(DataOverviewScene) || CheckScene(SpawnerScene))
+        if(CheckScene(Intro) || CheckScene(MainMenuScene) || CheckScene(AccountDataOverview) || CheckScene(SpawnerScene))
             return true;
         else return false;
     }
--- Assets/Project/ThirdPerson_Pre/UI_InGame/Scripts/UICanvas_SpawnerScene.cs
+++ Assets/Project/ThirdPerson_Pre/UI_InGame/Scripts/UICanvas_SpawnerScene.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,37 +8,100 @@ using UnityEngine.UI;
 public class UICanvas_SpawnerScene : MonoBehaviour
 {
     [SerializeField] Button BackToMainMenuBtton;
+    [SerializeField] Button StartGame;
     [SerializeField] Button JoinGameButton_1; // nut join khi da xem trang bi
     [SerializeField] Button JoinGameButton_2; // nut join khi da xem trang bi
+    [SerializeField] Button JoinGameButton_3; // nut join khi da xem trang bi
+
 
     [SerializeField] Button ChangeNextSkinsButton;
     [SerializeField] Button ChangePreviousSkinsButton;
 
-
+    [SerializeField] int mapSelectIndex;
+    [SerializeField] Transform mapSelectTranform;   // gameobject = content in scroll view| transform chua cac button select map
+    [SerializeField] GameObject[] maps;
     private void Awake() {
+        StartGame.onClick.AddListener(OnGameStart);
         JoinGameButton_1.onClick.AddListener(JoinGameButton_1_OnClick);
         JoinGameButton_2.onClick.AddListener(JoinGameButton_2_OnClick);
+        JoinGameButton_3.onClick.AddListener(JoinGameButton_3_OnClick);
+
 
         BackToMainMenuBtton.onClick.AddListener(BackToMainMenuBtton_OnClick);
         ChangeNextSkinsButton.onClick.AddListener(ChangeNextSkinsButton_OnClick);
         ChangePreviousSkinsButton.onClick.AddListener(ChangePreviousSkinsButton_OnClick);
+
+        mapSelectIndex = 0;
+        maps = new GameObject[mapSelectTranform.childCount];
+
+        
     }
 
+    private void Start() {
+        for (int i = 0; i < mapSelectTranform.childCount; i++)
+        {
+            maps[i] = mapSelectTranform.GetChild(i).gameObject;
+        }
+    }
 
-    void JoinGameButton_1_OnClick() {
+    private void OnGameStart()
+    {
+        // check mapSelectIndex -> goi dung scene
+        /* if(mapSelectIndex == 1) TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.Testing_ThirdPerson);
+        else if(mapSelectIndex == 2) TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.Testing_BattleRoyale); */
+
         Time.timeScale = 1; //todo BAT DAU GAME
-        //TestLoadingScene.Instance.LoadScene(TestLoadingScene.TestingThirdPerson_Scene);
-        TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.Testing_ThirdPerson);
+        switch (mapSelectIndex)
+        {
+            case 1:
+                TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.Testing_ThirdPerson);
+                break;
+            case 2: 
+                TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.Testing_BattleRoyale);
+                break;
+
+            default:
+                break;
+        }
+    }
+    void UpdataVisual(int mapSelectIndex) {
+        for (int i = 0; i < maps.Length; i++)
+        {
+            if(i == mapSelectIndex-1) {
+                maps[i].transform.GetChild(0).gameObject.SetActive(true);
+                
+            } else {
+                maps[i].transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
+    void JoinGameButton_1_OnClick() {
+        /* Time.timeScale = 1; //todo BAT DAU GAME
+        TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.Testing_ThirdPerson); */
+
+        mapSelectIndex = 1;
+        UpdataVisual(mapSelectIndex);
     }
     
     private void JoinGameButton_2_OnClick()
     {
-        Time.timeScale = 1; //todo BAT DAU GAME
-        TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.Testing_BattleRoyale);
+        /* Time.timeScale = 1; //todo BAT DAU GAME
+        TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.Testing_BattleRoyale); */
+
+        mapSelectIndex = 2;
+        UpdataVisual(mapSelectIndex);
+    }
+
+    private void JoinGameButton_3_OnClick()
+    {
+        /* Time.timeScale = 1; //todo BAT DAU GAME
+        TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.Testing_BattleRoyale); */
+
+        mapSelectIndex = 3;
+        UpdataVisual(mapSelectIndex);
     }
 
     void BackToMainMenuBtton_OnClick() {
-        //TestLoadingScene.Instance.LoadScene(TestLoadingScene.MainMenu_Scene);
         TestLoadingScene.Instance.LoadScene_Enum(TestLoadingScene.ScenesEnum.MainMenu);
     }
 
